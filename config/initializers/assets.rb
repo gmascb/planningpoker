cb3ae2e0b7fe060acef108c217e056fcbdb47eb5
Guilherme <gmascb@gmail.com>
2017-06-12 03:49:27 +0000

    NotifyJS e Calculos

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,4 +12,5 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( _form.scss
                                                   index.scss
                                                   show.scss
+                                                  notifyjs/notify.js
                                                   application.css)
