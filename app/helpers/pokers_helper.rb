8eeb35dc8d480be30368138b9d8b0754a051b18c
Guilherme <gmascb@gmail.com>
2017-08-04 02:56:41 +0000

    Adicionar Moda na visão de Pokers

--- app/helpers/pokers_helper.rb
+++ app/helpers/pokers_helper.rb
@@ -1,2 +1,13 @@
 module PokersHelper
+  
+  def moda(array, find_all=true)
+    histogram = array.inject(Hash.new(0)) { |h, n| h[n] += 1; h }
+    modes = nil
+    histogram.each_pair do |item, times|
+      modes << item if modes && times == modes[0] and find_all
+      modes = [times, item] if (!modes && times>1) or (modes && times>modes[0])
+    end
+    return modes ? modes[1..modes.size] : modes
+  end
+  
 end
