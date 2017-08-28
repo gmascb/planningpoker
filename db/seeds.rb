639683fae993c9b59fec164ab00bc0f3714cfebe
Guilherme <gmascb@gmail.com>
2017-08-28 23:35:44 +0000

    Refatorando, Pequenos ajustes css e rake seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+Poker.create(user: 'A', value: 5, room: 2)
+Poker.create(user: 'B', value: 1, room: 2)
+Poker.create(user: 'C', value: 2, room: 2)
+Poker.create(user: 'D', value: 3, room: 2)
+Poker.create(user: 'E', value: 5, room: 2)
+Poker.create(user: 'F', value: 5, room: 2)
+Poker.create(user: 'G', value: 1, room: 2)
+Poker.create(user: 'H', value: 2, room: 2)
+Poker.create(user: 'I', value: 5, room: 2)
