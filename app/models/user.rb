0d00f97a9c2f70f4fb260c475c6283aa4ee65235
Guilherme <gmascb@gmail.com>
2017-09-18 19:18:10 +0000

    Pequenos ajustes para o refreshAuto

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_initialize do |user|
       
@@ -6,17 +7,13 @@ class User < ActiveRecord::Base
       user.uid = auth.uid
       user.name = auth.info.name
       user.oauth_token = auth.credentials.token
-      
-      #raise auth.provider
+
       if user.provider != 'github'
         user.oauth_expires_at = Time.at(auth.credentials.expires_at)
       end
       
-      if user.provider == 'github'
-        #user.email = auth.info.email
-      end
-      
       user.save!
+      
     end
   end
 end
