7b4be8e32aac31bcc6b2b771fc5dd29135372785
Guilherme Mascarenhas Barbosa <gmascb@gmail.com>
2020-09-15 10:00:26 -0300

    fix last_login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,9 @@
 class SessionsController < ApplicationController
 
   def create
+    byebug
     user = User.from_omniauth(request.env["omniauth.auth"])
-    user.last_login = DateTime.now
+    user.update(last_login: DateTime.now)
     session[:user_id] = user.id
     redirect_to rooms_path(:filtrado => 1)
   end
