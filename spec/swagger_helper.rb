721e25d481d06f26437781324bd4a4148e45ff5a
Guilherme Barbosa <guilhermebarbosa@Guilhermes-MacBook-Pro.local>
2020-10-15 10:03:33 -0300

    swagger 3.0.1

--- spec/swagger_helper.rb
+++ spec/swagger_helper.rb
@@ -21,21 +21,29 @@ RSpec.configure do |config|
         title: 'API V1 Techpoker',
         version: 'v1'
       },
-      # securityDefinitions: {
-      #     Authorization: {
-      #         description: "Header for Authorization",
-      #         type: :apiKey,
-      #         name: 'Authorization',
-      #         in: :header
-      #     }
-      # },
+      securityDefinitions: {
+          Authorization: {
+              description: "Header for Authorization",
+              type: :apiKey,
+              name: 'Authorization',
+              in: :header
+          }
+      },
       # basePath: "/api",
       servers: [
         {
-          url: 'http://{defaultHost}',
+          url: 'http://{prodHost}',
           variables: {
-            defaultHost: {
-              default: Rails.env.production? ? 'techpoker.herokuapp.com/api' : "localhost:3000/api",
+            prodHost: {
+              default: 'techpoker.herokuapp.com/api'
+            },
+          }
+        },
+        {
+          url: 'http://{devHost}',
+          variables: {
+            devHost: {
+              default: "localhost:3000/api",
             }
           }
         }
