9fbc436ce3e1e1e3eac95e8bb4e81b09b21cc82c
Guilherme Barbosa <guilhermebarbosa@Guilhermes-MacBook-Pro.local>
2020-10-15 09:29:56 -0300

    rswagger

--- config/initializers/rswag-ui.rb
+++ config/initializers/rswag-ui.rb
@@ -1,15 +1,17 @@
-unless Rails.env.production?
-  Rswag::Ui.configure do |c|
+Rswag::Ui.configure do |c|
 
-    # List the Swagger endpoints that you want to be documented through the swagger-ui
-    # The first parameter is the path (absolute or relative to the UI host) to the corresponding
-    # endpoint and the second is a title that will be displayed in the document selector
-    # NOTE: If you're using rspec-api to expose Swagger files (under swagger_root) as JSON or YAML endpoints,
-    # then the list below should correspond to the relative paths for those endpoints
+  # List the Swagger endpoints that you want to be documented through the swagger-ui
+  # The first parameter is the path (absolute or relative to the UI host) to the corresponding
+  # endpoint and the second is a title that will be displayed in the document selector
+  # NOTE: If you're using rspec-api to expose Swagger files (under swagger_root) as JSON or YAML endpoints,
+  # then the list below should correspond to the relative paths for those endpoints
 
-    c.swagger_endpoint '/api-docs/v1/swagger.yaml', 'v1'
-    # c.swagger_endpoint '/api-docs/v2/swagger.yaml', 'v2'
+  c.swagger_endpoint '/api-docs/v1/swagger.yaml', 'API V1 TechPoker'
 
-    c.config_object['docExpansion'] = 'none'
-  end
+  # Add Basic Auth in case your API is private
+  # c.basic_auth_enabled = true
+  # c.basic_auth_credentials 'username', 'password'
+
+  c.config_object['docExpansion'] = 'none'
 end
+
--- spec/swagger_helper.rb
+++ spec/swagger_helper.rb
@@ -44,13 +44,30 @@ RSpec.configure do |config|
   # }
   config.swagger_docs = {
     'v1/swagger.yaml' => {
-        swagger: '2.0',
+      swagger: '2.0',
       info: {
         title: 'API V1',
         version: 'v1'
       },
       basePath: '/api',
-      paths: {}
+      paths: {},
+      # definitions: {
+      #   contact: {
+      #     type: :object,
+      #     properties: {
+      #       data: {
+      #         type: :object,
+      #         required: %i[name phone email address],
+      #         properties: {
+      #           name: { type: :string, example: 'magmaLabs.io' },
+      #           phone: { type: :string, example: '+52 1 667 317 9035' },
+      #           email: { type: :string, example: 'hello@magmalabs.io' },
+      #           address: { type: :string, example: 'Av. Constitución #2035. Colima, Colima, MX, 28017' }
+      #         }
+      #       }
+      #     }
+      #   }
+      # }
     }
   }
 
